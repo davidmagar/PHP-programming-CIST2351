803bf81846c78f47b12bf6225bf9535ee330ae39
David Ghalley <dillighalley2012@gmail.com>
2022-02-22 20:19:57 -0500

    Add files via upload

---
+++ exercise 2.1/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Exercise 2-1</title>
+</head>
+<body>
+	<?php 
+		$class = "PHP Programming 1";
+		$course = "CIST";
+		$course_number = 2351;
+		$exercise = 2.1;
+
+		echo "This is $class. <br>
+		The course number is $course$course_number. <br>
+		I am working on chapter 2 exercise $exercise."  
+
+	?>
+
+</body>
+</html>
